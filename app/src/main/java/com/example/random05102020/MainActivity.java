dc7eb30951c1442c055a0afb2b76c26daff6e888
phatdroid94 <phatdroid94@gmail.com>
2020-10-16 21:19:52 +0700

    random

--- app/src/main/java/com/example/random05102020/MainActivity.java
+++ app/src/main/java/com/example/random05102020/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,8 +28,36 @@ public class MainActivity extends AppCompatActivity{
         //Log.d("BBB",mTv.getText().toString());
 
         // Hàm Thông báo
-        Toast
-                .makeText(MainActivity.this , "Hello" , Toast.LENGTH_SHORT)
-                .show();
+//        Toast
+//                .makeText(MainActivity.this , "Hello" , Toast.LENGTH_SHORT)
+//                .show();
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+//        on vs emit
+//        mBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        mTv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "textview", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        // built in function : Log.d() , Math
+        // Làm tròn
+        //float num1 =  Math.round(1.5f);
+        // Làm tròn xuống : floor (Sàn nhà)
+        //double num2 = Math.floor(1.9999999);
+        // Làm tròn lên : ceil (trần nhà)
+        //double num3 = Math.ceil(1.01);
+        // random
+        // 0 - 5
+//        for (int i = 0; i < 100 ; i++) {
+//            double num4 = Math.floor(Math.random() * 6);
+//            Log.d("BBB",num4 + "");
+//        }
+//        Toast.makeText(this, num4 + "", Toast.LENGTH_SHORT).show();
     }
 }
